cdd5fc4c9e31af7ee8d2f17ab5ea53ba7a373c28
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2025-05-06 19:23:03 +0300

    feat: Integrate Firebase into the project with necessary configurations and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:happyfarm/happy_farm.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 ///  Easy Localization
 /// dart run easy_localization:generate -S assets/translations
 /// dart run easy_localization:generate -f keys -o locale_keys.g.dart -S assets/translations
@@ -9,6 +10,9 @@ import 'package:happyfarm/happy_farm.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
 
   runApp(
     EasyLocalization(
