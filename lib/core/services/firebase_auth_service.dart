884953749c6a5666e01b524b2930cf48e62d7986
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2025-05-06 20:19:04 +0300

    feat: Implement Firebase Authentication Service and Custom Exception Handling
    - Added CustomException class for better error handling.
    - Created FirebaseAuthService to manage user authentication with Firebase.
    - Implemented createUserWithEmailAndPassword method with detailed error handling using CustomException.
    - Added UserModel to map Firebase User to application-specific user entity.
    - Updated project configuration files for CMake and build settings.

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:happyfarm/core/errors/exeptions.dart';
+
+class FirebaseAuthService  {
+  Future<User> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+       throw CustomException( 'The password provided is too weak.' );
+      } else if (e.code == 'email-already-in-use') {
+     throw CustomException( "this account alredy exists for that email" );
+      } else if (e.code == 'invalid-email') {
+        throw CustomException( 'The email address is badly formatted.' );
+      } else if (e.code == 'operation-not-allowed') {
+        throw CustomException( 'The operation is not allowed.' );
+      } else if (e.code == 'user-disabled') {
+        throw CustomException( 'The user account has been disabled by an administrator.' );
+      } else if (e.code == 'user-not-found') {
+        throw CustomException( 'No user found for that email.' );
+      } else if (e.code == 'wrong-password') {
+        throw CustomException( 'Wrong password provided for that user.' );
+      } else {
+        throw CustomException( e.message! );
+      }
+    } catch (e) {
+      throw CustomException( e.toString() );  
+    }
+  }
+}
--- lib/features/auth/data/repos/auth_repo_implementation.dart
+++ lib/features/auth/data/repos/auth_repo_implementation.dart
@@ -1,12 +1,32 @@
- import 'package:dartz/dartz.dart';
+import 'package:dartz/dartz.dart';
+import 'package:happyfarm/core/errors/exeptions.dart';
 import 'package:happyfarm/core/errors/failures.dart';
+import 'package:happyfarm/core/services/firebase_auth_service.dart';
+import 'package:happyfarm/features/auth/data/models/user_model.dart';
+
 import 'package:happyfarm/features/auth/domain/repos/auth_repo.dart';
 import 'package:happyfarm/features/auth/domain/repos/entites/user_entity.dart';
 
-class AuthRepoImplementation  extends AuthRepo  {
+class AuthRepoImplementation extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+  AuthRepoImplementation(this.firebaseAuthService);
   @override
-  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({required String email, required String password}) {
-    // TODO: implement createUserWithEmailAndPassword
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      {required String email,
+      required String password,
+      required String name}) async {
+    try {
+      final user = await firebaseAuthService.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return Right(
+        UserModel.fromFirebaseUser(user),
+      );
+    } on CustomException catch (e) {
+      return Left(ServerFailure(e.message));
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
   }
 }
