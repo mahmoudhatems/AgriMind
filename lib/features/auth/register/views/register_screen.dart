0c1b5eaa29fbc3b2fe7a4f2a02b2a41845f436ec
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2025-06-25 00:05:56 +0300

    Refactor authentication flow in Happy Farm project: Introduced MultiBlocProvider and MultiBlocListener in the registration screen to manage both signup and login states. Enhanced user experience with loading overlays during authentication processes and improved toast notifications for success and failure states.

--- lib/features/auth/register/views/register_screen.dart
+++ lib/features/auth/register/views/register_screen.dart
@@ -4,6 +4,7 @@ import 'package:happyfarm/core/services/get_it__service.dart';
 import 'package:happyfarm/core/utils/colors.dart';
 import 'package:happyfarm/core/widgets/app_bar.dart';
 import 'package:happyfarm/features/auth/cubits/signup_cubits/signup_cubit.dart';
+import 'package:happyfarm/features/auth/cubits/login_cubits/login_cubit.dart';
 import 'package:happyfarm/features/auth/domain/repos/auth_repo.dart';
 import 'package:happyfarm/features/auth/register/widgets/signup_body_consumer.dart';
 
@@ -12,22 +13,24 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SignupCubit(getIt<AuthRepo>()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SignupCubit>(
+          create: (_) => SignupCubit(getIt<AuthRepo>()),
+        ),
+        BlocProvider<LoginCubit>(
+          create: (_) => LoginCubit(getIt<AuthRepo>()),
+        ),
+      ],
       child: Scaffold(
         backgroundColor: ColorsManager.realWhiteColor,
         appBar: buildAppBar(
           title: "Register",
           context: context,
         ),
-        body: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              /// Register Buttons
-              SignupViewBodyBlocConsumer(),
-            ],
-          ),
+        body: const SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: SignupViewBodyBlocConsumer(),
         ),
       ),
     );
